ed8750b123a6cb3146a26997c7d18826418ba71a
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-14 19:15:19 +0900

    when checked, button is disabled

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
+  const [isDisable, setIsDisable] = useState(false);
+
   return (
     <div>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={isDisable}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setIsDisable(e.target.checked);
+        }}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -15,3 +15,24 @@ test("버튼의 초기색상과 클릭시에 변경된 색상 테스트", () => 
 
   expect(colorButton).toHaveTextContent("Change to red");
 });
+
+test("초기 조건", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("체크박스 선택시 버튼 비활성화", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button");
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).not.toBeDisabled();
+});
